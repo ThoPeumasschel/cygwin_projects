ec394e29ea3f6b0fb5f282de773575c4a7e88f76
Thomas Peuschel <tpeuschel@web.de>
2018-04-25 11:47:10 +0100

    kjhsdf eswv

---
+++ lcthw/ex3.c
@@ -0,0 +1,11 @@
+
+int main(int argc, char *argv[])
+{
+    int age = 100;
+    int height = 72;
+
+    printf("I am %d years old.\n");
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
